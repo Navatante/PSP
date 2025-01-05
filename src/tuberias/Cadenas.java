af6e140e69320dbaeaac5daad24dac1901c4b2de
Jon Carbonell <jonatancarmar@gmail.com>
2025-01-05 08:56:11 +0100

    Tuberias

---
+++ src/tuberias/Cadenas.java
@@ -0,0 +1,31 @@
+package tuberias;
+
+import java.util.Random;
+
+public class Cadenas {
+    public static void main(String[] args) {
+
+        generarCadenasConSilabas(Integer.parseInt(args[0]));
+
+    }
+
+    public static void generarCadenasConSilabas(int cantidadCadenas) {
+        Random random = new Random();
+        String[] silabas = {"ma", "te", "lo", "vi", "ca", "re", "du", "fo", "sa", "ni", "co", "lu", "ba", "qu", "ta", "ne", "ra", "pu", "di", "su"};
+
+
+        for (int i = 0; i < cantidadCadenas; i++) {
+            int longitudCadena = random.nextInt(10) + 1;            // nextInt(10), me da un rango entre 0-9, entonces sumandole 1 al rango, me queda del 1-10.
+            longitudCadena /= 2;                                    // lo reduzco a la mitad, porque las silabas contienen dos caracteres
+            StringBuilder cadenaDeCaracteres = new StringBuilder();
+
+            for (int j = 0; j < longitudCadena; j++) {
+                int num = random.nextInt(silabas.length-1);         // .length da el numero de elementos, en este caso 20. por eso le resto uno, porque el maximo indice es el 19, al empezar por 0
+                cadenaDeCaracteres.append(silabas[num]);
+            }
+
+            System.out.println(cadenaDeCaracteres.toString());
+        }
+    }
+}
+
---
+++ src/tuberias/Frecuencia.java
@@ -0,0 +1,41 @@
+package tuberias;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Frecuencia {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);   // InputStreamReader solo le caracter a caracter. (aqui estamos diciendo: leeme caracter a caracter de la consola "System.in")
+        BufferedReader lector = new BufferedReader(isr);            // Gracias a BufferedReader leemos cadenas de caracteres enteras. (aqui estamos diciendo: leeme todos los caracteres de "isr" del tiron)
+
+        String linea;
+        while ((linea = lector.readLine()) != null) {
+            contarVocales(linea);
+        }
+    }
+
+    public static void contarVocales(String cadena) {
+        HashMap<Character, Integer> frecuenciaVocales = new HashMap<>();
+        frecuenciaVocales.put('a', 0);
+        frecuenciaVocales.put('e', 0);
+        frecuenciaVocales.put('i', 0);
+        frecuenciaVocales.put('o', 0);
+        frecuenciaVocales.put('u', 0);
+
+        for (char c : cadena.toLowerCase().toCharArray()) {
+            if (frecuenciaVocales.containsKey(c)) {
+                frecuenciaVocales.put(c, frecuenciaVocales.get(c) + 1);
+            }
+        }
+
+        System.out.printf("Cadena: '%s' --> %d %d %d %d %d\n",
+                cadena,
+                frecuenciaVocales.get('a'),
+                frecuenciaVocales.get('e'),
+                frecuenciaVocales.get('i'),
+                frecuenciaVocales.get('o'),
+                frecuenciaVocales.get('u'));
+    }
+}
